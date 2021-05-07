7edf846cdfcde58edee35f0bc4104ea1b4abcec4
ToshBoss <mailtojoshuaphilip@gmail.com>
2021-05-07 10:22:41 -0400

    yessss

--- main.ts
+++ main.ts
@@ -14,6 +14,24 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     isCovid = 1
 })
+radio.onReceivedString(function (receivedString) {
+    //let temp = JSON.parse(receivedString)
+    let temp = receivedString.split(",")
+    
+
+    
+serial.writeLine("DEAR GOD" + temp.length)
+
+    temp.forEach(function (value) {
+        if(list.indexOf(temp[i]) == -1){
+        list.push(temp[i])
+        }
+        basic.showString("" + (temp[i]))
+        basic.pause(1000)
+        basic.clearScreen()
+    }); 
+
+})
 radio.onReceivedValue(function (name, value) {
     signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
     if (control2) {
@@ -26,13 +44,13 @@ radio.onReceivedValue(function (name, value) {
         tempId = value
         if (Math.map(signal, -95, -42, 0, 9) > 3.87) {
             serial.writeLine("TOO CLOSE TO " + value)
-            if (list.indexOf(value) == -1) {
-                list.push(value)
+            if (list.indexOf(value.toString()) == -1) {
+                list.push(value.toString())
             }
         }
     }
     if (name == "isCovid") {
-        if (value == 1 && list.indexOf(tempId) != -1) {
+        if (value == 1 && list.indexOf(tempId.toString()) != -1) {
             isCovid = 1
             basic.showLeds(`
                 # . . . #
@@ -50,16 +68,19 @@ radio.onReceivedValue(function (name, value) {
 let tempId = 0
 let signal = 0
 let control2 = false
-let list: number[] = []
 let isCovid = 0
+let list: string[] = []
+let i = 0
 let id = control.deviceSerialNumber()
 isCovid = 0
 list = []
 control2 = true
+list.push(control.deviceSerialNumber().toString())
+list.push("0")
 radio.setGroup(1)
 radio.setTransmitPower(1)
 basic.forever(function () {
-    radio.sendValue("id", id)
     radio.sendValue("isCovid", isCovid)
+    radio.sendString("" + (list.join()))
     basic.pause(200)
 })

2367856c7c7d7dc59372d99a8d657a90a6e12c01
ToshBoss <mailtojoshuaphilip@gmail.com>
2021-06-03 11:21:10 -0400

    Functioning Code

--- main.ts
+++ main.ts
@@ -15,44 +15,29 @@ input.onButtonPressed(Button.AB, function () {
     isCovid = 1
 })
 radio.onReceivedString(function (receivedString) {
-    //let temp = JSON.parse(receivedString)
-    let temp = receivedString.split(",")
-    
-
-    
-    //serial.writeLine("DEAR GOD" + temp.length)
-
-    temp.forEach(function (value) {
-        if(list.indexOf(temp[i]) == -1){
-            list.push(temp[i])
-            serial.writeLine("hewwo " + temp[i])
-            
-        }
-        // basic.showString("" + (temp[i]))
-        // basic.pause(1000)
-        // basic.clearScreen()
-    }); 
+    // let temp = JSON.parse(receivedString)
+    if (Math.map(signal, -95, -42, 0, 9) > 3.87) {
+        temp = receivedString.split(",")
+        temp.forEach(function (value) {
+            if(list.indexOf(value) == -1){
+                list.push(value)
+                serial.writeLine("hewwo " + value)   
+            }
+        });
+    }
 
 })
 radio.onReceivedValue(function (name, value) {
     signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
     if (control2) {
         led.plotBarGraph(
-        Math.map(signal, -95, -42, 0, 9),
-        9
-        )
+        Math.map(signal, -95, -42, 0, 9),9)
     }
-    if (name == "id") {
-        tempId = value
-        if (Math.map(signal, -95, -42, 0, 9) > 3.87) {
-            serial.writeLine("TOO CLOSE TO " + value)
-            if (list.indexOf(value.toString()) == -1) {
-                list.push(value.toString())
-            }
-        }
+    if(name == 'id'){
+        tempId = value;
     }
     if (name == "isCovid") {
-        if (value == 1 && list.indexOf(tempId.toString()) != -1) {
+        if (value == 1 && list.indexOf(("" + tempId)) != -1) {
             isCovid = 1
             basic.showLeds(`
                 # . . . #
@@ -67,23 +52,25 @@ radio.onReceivedValue(function (name, value) {
         }
     }
 })
-let tempId = 0
 let signal = 0
+let temp: string[] = []
 let control2 = false
 let isCovid = 0
-let list: string[] = []
 let i = 0
+let list: string[] = []
+let tempId = 0
 let id = control.deviceSerialNumber()
 isCovid = 0
 list = []
 control2 = true
-let tempList: string[] = [control.deviceSerialNumber().toString(), "false"]
+let tempList = [control.deviceSerialNumber().toString(), "false"]
 list.push(tempList.join())
-//list.push("0")
+// list.push("0")
 radio.setGroup(1)
 radio.setTransmitPower(1)
 basic.forever(function () {
     radio.sendValue("isCovid", isCovid)
-    radio.sendString((list.join()))
+    radio.sendValue("id", id)
+    radio.sendString("" + (list.join()))
     basic.pause(200)
 })

7fb3269e9599a22d4365b7e764f69ee3f358a62f
ToshBoss <mailtojoshuaphilip@gmail.com>
2021-06-15 13:05:09 -0400

    hi

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 enum RadioMessage {
     message1 = 49434
 }
-
-//Display List of People that Have been in close contact recently WITH ANYONE. 
+// Display List of People that Have been in close contact recently WITH ANYONE.
 input.onButtonPressed(Button.A, function () {
     control2 = false
     basic.clearScreen()
@@ -13,46 +12,64 @@ input.onButtonPressed(Button.A, function () {
     }
     control2 = true
 })
-
-//Broadcast a positive case.
+// Broadcast a positive case.
 input.onButtonPressed(Button.AB, function () {
     isCovid = 1
+    globalList.forEach(function (val) {
+        if(val.includes(""+ control.deviceSerialNumber())){
+            globalList.removeAt(globalList.indexOf(val))
+            globalList.push(""+control.deviceSerialNumber() + ", true");
+            serial.writeLine("WROTE SMTH")
+        }
+    });
 })
-
-//When recieved the global list, update any new entries and broadcast
+// When recieved the global list, update any new entries and broadcast
 radio.onReceivedString(function (receivedString) {
+    //serial.writeLine("HELP" + receivedString)
     // let temp = JSON.parse(receivedString)
     if (true) {
+        let ff = false;
         temp = receivedString.split(",")
-        temp.forEach(function (value) {
-            if(globalList.indexOf(value) == -1){
-                globalList.push(value)
-                serial.writeLine("hewwo " + value)   
+        if(!globalList.join().includes(receivedString)){
+            globalList.push(receivedString)
+        }
+        globalList.forEach(function (val) {
+            let x = val.split()
+            serial.writeLine("PUSHING" + val)
+
+            
+            if(x[1] == "true" && (closeList.join().includes(x[0]))){
+                globalList.removeAt(globalList.indexOf(val))
+                globalList.push(""+control.deviceSerialNumber() + ", true");
+                serial.writeLine("WROTE SMTH")
+                //closeList.removeAt(closeList.indexOf(val))
+                serial.writeLine("PUSHING" + val)
+                closeList.push(val);
             }
-        });
+         }); 
+               closeList.forEach(function (val) {
+                   serial.writeLine("CL:OSE" + val)
+         });        
     }
-
 })
-
-//show bar graph output
+// show bar graph output
 radio.onReceivedValue(function (name, value) {
     signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
     if (control2) {
         led.plotBarGraph(
-        Math.map(signal, -95, -42, 0, 9),9)
+        Math.map(signal, -95, -42, 0, 9),
+        9
+        )
     }
-    //check if closest contact has had Covid to see if prev. exposed
-    if(name == 'id'){
-        serial.writeLine("HELLO" + value)
-        tempId = value;
-      if (Math.map(signal, -95, -42, 0, 9) > 3.87) {
-            closeList.push("" + value);
+    // check if closest contact has had Covid to see if prev. exposed
+    if (name == "id") {
+        //serial.writeLine("HELLO" + value)
+        tempId = value
+        if (Math.map(signal, -95, -42, 0, 9) > 3.87 && closeList.indexOf(("" + value + ", false" )) == -1){
+            closeList.push("" + value + ", false")
         }
     }
-    
-    //??
-    if (name == "isCovid") {
-        if (value == 1 && closeList.indexOf(("" + tempId)) != -1) {
+        if (closeList.join().includes("true")) {
             isCovid = 1
             basic.showLeds(`
                 # . . . #
@@ -65,22 +82,22 @@ radio.onReceivedValue(function (name, value) {
             basic.showString(convertToText(globalList[0]).substr(0, 1))
             basic.pause(1000)
         }
-    }
 })
+let tempId = 0
 let signal = 0
-let temp: string[] = []
 let control2 = false
 let isCovid = 0
-let i = 0
 let globalList: string[] = []
-let closeList: string[] = []
-
-let tempId = 0
+let i = 0
+let temp: string[] = []
 let id = control.deviceSerialNumber()
 isCovid = 0
 globalList = []
+let closeList: string[] = []
+
 control2 = true
-let tempList = [control.deviceSerialNumber().toString()]
+let aList = [control.deviceSerialNumber().toString(), "false"]
+let tempList = [aList.join()]
 globalList.push(tempList.join())
 // list.push("0")
 radio.setGroup(1)
